6819b7e4343bf9d6de9eb2c31dcd1cc40f773fbb
Chris Tandiono <chris@christandiono.com>
2012-04-21 07:38:18 -0700

    derp

--- bookmarks/base/views.py
+++ bookmarks/base/views.py
@@ -14,7 +14,6 @@ def index(request):
 @login_required
 def my_bookmarks(request):
     bookmarks = Bookmark.objects.filter(user=request.user).order_by('-date')
-    for mark in bookmarks:
     real_bookmarks = []
     bad = 0
     for mark in bookmarks:
