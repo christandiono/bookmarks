70d7470cbcb67bde37948f96742430a5184fb205
Chris Tandiono <chris@christandiono.com>
2012-04-21 01:49:30 -0700

    herp a derp

--- bookmarks/base/models.py
+++ bookmarks/base/models.py
@@ -9,6 +9,12 @@ class Bookmark(models.Model):
     user = models.ForeignKey(User)
     fb_id = models.TextField() # ?!?!
 
+    def get_url(self):
+        return "https://facebook.com/posts/%s" % fb_id
+
+    def __unicode__(self):
+        return unicode(fb_id)
+
 class UserFeed(models.Model): # authenticates a user to read a certain feed
     user = models.ForeignKey(User)
     feed_id = models.TextField(default=0)
