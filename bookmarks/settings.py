db99405f92a9b019a39c6086f06aeb9f7a8893f0
Chris Tandiono <chris@christandiono.com>
2012-04-20 22:01:44 -0700

    finally fix this

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -94,6 +94,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages"
+    "django.core.context_processors.request",
 )
 
 # List of callables that know how to import templates from various sources.
