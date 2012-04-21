bd296799f38694c84ab676b65e0e135b151f5424
Chris Tandiono <chris@christandiono.com>
2012-04-20 21:32:53 -0700

    attempt to fix

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -85,6 +85,17 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'vk!*)(0hcvszso8^6!)6!j3n$6xb8tz_8s5%r*+=+rnsel@xls'
 
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+)
+
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
