51694f35ee532f18d83d7edcabffd1525f133b20
Chris Tandiono <chris@christandiono.com>
2012-04-21 04:02:56 -0700

    derp

--- bookmarks/base/context_processors.py
+++ bookmarks/base/context_processors.py
@@ -3,7 +3,7 @@ import uuid
 def feed_id_processor(request):
     if request.user.is_authenticated():
         uf = UserFeed.objects.filter(user=request.user)
-        if !uf.exists():
+        if not uf.exists():
             uf = UserFeed(user=request.user, feed_id=uuid.uuid4().hex)
             uf.save()
         else:
