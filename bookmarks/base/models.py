215a225b307d5072e584318b70e559610a95a09d
Chris Tandiono <chris@christandiono.com>
2012-04-20 21:07:32 -0700

    forgot colon...

--- bookmarks/base/models.py
+++ bookmarks/base/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Bookmark(models.Model)
+class Bookmark(models.Model):
     status_id = models.IntegerField()
     comment_id = models.IntegerField()
