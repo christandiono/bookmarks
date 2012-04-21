d2bfcfe3ffc7168950a0630cd197f58972aa77dc
Chris Tandiono <chris@christandiono.com>
2012-04-20 21:33:45 -0700

    fix more stuff

--- bookmarks/base/models.py
+++ bookmarks/base/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 
 class Bookmark(models.Model):
+    user = models.ForeignKey(User)
     status_id = models.IntegerField()
     comment_id = models.IntegerField()
