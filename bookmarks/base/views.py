ac13c38074322fea5f7ba88fdebe2dfc48b2b6e0
Chris Tandiono <chris@christandiono.com>
2012-04-20 21:15:48 -0700

    fix broken import

--- bookmarks/base/views.py
+++ bookmarks/base/views.py
@@ -2,6 +2,7 @@
 
 from models import Bookmark
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
