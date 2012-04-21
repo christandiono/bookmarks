5ad0849ae74081969480a02343c6dea737bc324d
Chris Tandiono <chris@christandiono.com>
2012-04-20 20:04:18 -0700

    stupid heroku

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -150,3 +150,41 @@ AUTHENTICATION_BACKENDS = ('facebook.auth.backends.FacebookBackend',)
 
 FACEBOOK_APP_ID = '438195499528827'
 FACEBOOK_APP_SECRET = '9ecf505a5c850e26ce5c1ed9cbe1b0b7'
+
+import os
+import sys
+import urlparse
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
