9a0228993341c00d736a74efff7171988df6555e
Chris Tandiono <chris@christandiono.com>
2012-04-20 20:53:21 -0700

    fix

--- bookmarks/facebook/auth/backends.py
+++ bookmarks/facebook/auth/backends.py
@@ -39,8 +39,7 @@ class FacebookBackend(ModelBackend):
         # Download the user profile and cache a local instance of the
         # basic profile info
         response = urllib2.urlopen(
-            "https://graph.facebook.com/me/",
-            urllib.urlencode(dict(access_token=access_token)))
+            "https://graph.facebook.com/me/?" + urllib.urlencode(dict(access_token=access_token)))
         profile = json.loads(response.read())
         user = User(id=str(profile["id"]),
                     username=str(profile["id"]),
